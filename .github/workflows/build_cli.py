2b133cb7265d20f5fff3c6931875c208f359a8dc
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2025-12-03 20:23:43 +0800

    Update build_cli.py

--- .github/workflows/build_cli.py
+++ .github/workflows/build_cli.py
@@ -16,7 +16,7 @@ if not os.access(msbuildPath, os.X_OK):
 
 
 p = subprocess.run(
-    f'"{msbuildPath}" Magpie.slnx -m -t:Rebuild -restore -p:RestorePackagesConfig=true;Configuration=Release;Platform=x64;DisablePDB=true;OutDir={os.getcwd()}\\publish\\x64\\;'
+    f'"{msbuildPath}" Magpie.slnx -m -t:Rebuild -restore -p:RestorePackagesConfig=true;Configuration=Release;Platform=x64;DisablePDB=true;OutDir={os.getcwd()}\\publish\\x64\\'
 )
 subprocess.run(
     f'"{msbuildPath}" -restore -p:RestorePackagesConfig=true;Configuration=Release;Platform=x64;OutDir={os.getcwd()}\\publish\\x64\\ -t:Magpie_Core;Effects;CLI Magpie.sln'
